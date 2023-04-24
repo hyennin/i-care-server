6b19343e1e6f65aac55bfb2806430a281cca85bc
hyennin <chm050202@gmail.com>
2023-04-24 11:17:48 +0900

    불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './page/Login';
 import SignUp from './page/SignUp';
 import Community from './page/community/CommunityList';
-import Tip from './page/tip/Tip';
-import Diary from './page/diary/Diary';
-import MyPage from './page/MyPage';
 
 function App() {
   const [mode, setMode] = useState("");
@@ -37,18 +33,6 @@ function App() {
   return (
     <>
       {content}
-      {/* <Router>
-        <Switch>
-          <Route path='/' exact component={Community} />
-          <Route path='/community' component={Community} />
-          <Route path='/tip' component={Tip} />
-          <Route path='/diary' component={Diary} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/mypage' component={MyPage}/>
-          <Route path="*" element={<h1>404 페이지</h1>}/>
-        </Switch>
-      </Router> */}
     </>
   );
 }
