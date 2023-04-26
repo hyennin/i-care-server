f14d8c30896bf4e587d8d1b18267502b38b554af
hyennin <chm050202@gmail.com>
2023-04-27 07:30:53 +0900

    필요없는 코드 제거 및 수정

--- routes.js
+++ routes.js
@@ -15,14 +15,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 router.get('/authcheck', (req, res) => {
-	const sendData = { isLogin: "" };
-    if (req.session.is_logined) sendData.isLogin = "True"
-    else sendData.isLogin = "False"
+	const sendData = { isLogin: false };
+    if (req.session.is_logined) sendData.isLogin = true;
     res.send(sendData);
 });
 
 router.get('/logout', (req, res) => {
-    req.session.destroy(function (err) {
+    req.session.destroy((err) => {
         res.redirect('/');
     });
 });
